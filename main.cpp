18c7928178a0a818b8d909965e010fb1d7524a8a
Maeyanie <me@maeyanie.com>
2016-02-12 22:44:57 -0500

    Drop root privileges after setting up wiringPi, allows binary to be setuid root.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
+#if __linux__
+#include <sys/types.h>
+#include <unistd.h>
+#endif
 #include "mainwindow.h"
 #include "config.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+#if __linux__
+    qint32 uid = getuid();
+    qint32 euid = geteuid();
+    printf("Running as UID %d, EUID %d\n", uid, euid);
+#endif
 
     wiringPiSetupGpio();
     pinMode(ONOFFPIN, OUTPUT);
 
+#if __linux__
+    setuid(uid);
+    printf("Privileges dropped.\n");
+#endif
+
+    QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
