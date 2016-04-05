2c4c87ce5cd2adb152bd3c43b3e015b592bd82b4
Maeyanie <me@maeyanie.com>
2016-04-04 20:19:33 -0400

    Implement I2C Servo output

---
+++ out_servo.cpp
@@ -0,0 +1,39 @@
+#include <QTypeInfo>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <cstdio>
+#include "pidthread.h"
+
+#define SERVO 0x40
+
+Output_Servo::Output_Servo() {
+    dev = wiringPiI2CSetup(SERVO);
+    if (dev == -1) { fprintf(stderr, "Error opening I2C servo controller: %m\n"); exit(1); }
+
+    // Servo wants 50 Hz
+    // prescale = round(25 MHz / (4096 * 50 Hz))-1
+    wiringPiI2CWriteReg8(dev, 0xFE, 121);
+}
+
+void Output_Servo::on() {
+    wiringPiI2CWriteReg8(dev, 0x08, 0);
+    wiringPiI2CWriteReg8(dev, 0x09, 0);
+    wiringPiI2CWriteReg8(dev, 0x00, 0b00000001);
+    delay(1);
+}
+void Output_Servo::off() {
+    wiringPiI2CWriteReg8(dev, 0x00, 0b00010001);
+}
+
+void Output_Servo::set(double value) {
+    // 100 = 2 ms pulse
+    // 50 = 1.5 ms pulse
+    // 0 = 1 ms pulse
+
+    // each PWM is 20 ms
+    // 1 ms is 204.8 counts
+    qint32 t = (qint32)(value * 2.048 + 204.8 + 0.5);
+
+    wiringPiI2CWriteReg8(dev, 0x08, (t & 0xFF));
+    wiringPiI2CWriteReg8(dev, 0x09, (t & 0xFF00) >> 8);
+}
