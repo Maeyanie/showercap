7b438143044ef328323e998a00ec0200663663ec
Maeyanie <me@maeyanie.com>
2017-07-11 01:10:14 -0400

    Comment out test compilation from build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 qmake &&
 make &&
 sudo chown root.root Shower &&
-sudo chmod u+s Shower &&
+sudo chmod u+s Shower
 
-g++ -Wall -fPIE -o test test.cpp in_thermistor.o out_stepper.o \
-	-I/usr/include/arm-linux-gnueabihf/qt5 -I/usr/include/arm-linux-gnueabihf/qt5/QtCore -lwiringPi
+#g++ -Wall -fPIE -o test test.cpp in_thermistor.o out_stepper.o \
+#	-I/usr/include/arm-linux-gnueabihf/qt5 -I/usr/include/arm-linux-gnueabihf/qt5/QtCore -lwiringPi
 
 #g++ -Wall -fPIE -o manual manual.cpp in_thermistor.o out_stepper.o \
 #	-I/usr/include/arm-linux-gnueabihf/qt5 -I/usr/include/arm-linux-gnueabihf/qt5/QtCore -lwiringPi -lpthread -lncurses
